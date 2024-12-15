a2a067963c688df732588aaaabf47a01b127d6dd
Isez98 <isacc1798@gmail.com>
2024-12-15 01:24:04 -0700

    Resolved email sender port issue

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -63,7 +63,7 @@ func main() {
 	flag.BoolVar(&cfg.limiter.enabled, "limiter enabled", true, "Enable rate limiter")
 
 	flag.StringVar(&cfg.smtp.host, "smtp-host", "sandbox.smtp.mailtrap.io", "SMTP host")
-	flag.IntVar(&cfg.smtp.port, "smtp-port", 25, "SMTP port")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 2525, "SMTP port")
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "0b23c4503bf901", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "f3aa5affb8fef4", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.isez.dev>", "SMTP sender")
