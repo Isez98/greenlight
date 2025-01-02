6d42b025870ae6fdee47723ae493926e24b29e87
Isez98 <isacc1798@gmail.com>
2025-01-02 11:34:46 -0700

    Improve test app

--- cmd/api/testutils_test.go
+++ cmd/api/testutils_test.go
@@ -2,66 +2,42 @@ package main
 
 import (
 	"bytes"
-	"database/sql"
 	"flag"
 	"io"
 	"log/slog"
 	"net/http"
+	"net/http/cookiejar"
 	"net/http/httptest"
-	"os"
+	"strings"
 	"testing"
+	"time"
 
 	"greenlight.isez.dev/internal/data/mocks"
 )
-
-func newTestDB(t *testing.T) *sql.DB {
-	db, err := sql.Open("postgres", os.Getenv("TEST_GREENLIGHT_DB"))
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	script, err := os.ReadFile("./testdata/setup.sql")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	_, err = db.Exec(string(script))
-	if err != nil {
-		db.Close()
-		t.Fatal(err)
-	}
-
-	t.Cleanup(func() {
-		defer db.Close()
-
-		script, err := os.ReadFile("./testdata/teardown.sql")
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		_, err = db.Exec(string(script))
-		if err != nil {
-			t.Fatal(err)
-		}
-	})
-
-	return db
-}
 
 func newTestApplication(t *testing.T) *application {
 	var cfg config
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
 
-	db, err := openDB(cfg)
-	if err != nil {
-		os.Exit(1)
-	}
-	defer db.Close()
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.DurationVar(&cfg.db.maxIdleTime, "db-max-idle-time", 15*time.Minute, "PostgreSQL max connection idle time")
+
+	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
+	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
+	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
+
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
+
+	flag.Parse()
 
 	return &application{
 		logger: slog.New(slog.NewTextHandler(io.Discard, nil)),
 		config: cfg,
-		models: mocks.TestModels_Mock(db),
+		models: mocks.TestModels_Mock(),
 	}
 }
 
@@ -71,6 +47,18 @@ type testServer struct {
 
 func newTestServer(t *testing.T, h http.Handler) *testServer {
 	ts := httptest.NewTLSServer(h)
+
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ts.Client().Jar = jar
+
+	ts.Client().CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+
 	return &testServer{ts}
 }
 
--- internal/data/mocks/models_mock.go
+++ internal/data/mocks/models_mock.go
@@ -1,16 +1,14 @@
 package mocks
 
 import (
-	"database/sql"
-
 	"greenlight.isez.dev/internal/data"
 )
 
-func TestModels_Mock(db *sql.DB) data.Models {
+func TestModels_Mock() data.Models {
 	return data.Models{
-		Movies:      MovieModel_Mock{DB: db},
-		Permissions: PermissionModel_Mock{DB: db},
-		Tokens:      TokenModel_Mock{DB: db},
-		Users:       UserModel_Mock{DB: db},
+		Movies:      MovieModel_Mock{},
+		Permissions: PermissionModel_Mock{},
+		Tokens:      TokenModel_Mock{},
+		Users:       UserModel_Mock{},
 	}
 }
